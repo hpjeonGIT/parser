964d96188514f3eb69c01e6ed7d8f7d6899699da
Byoungseon Jeon <bjeon11@gmail.com>
2020-03-16 17:01:34 -0400

    Overriding listener
    Inherit exitField and customize data structure

--- antlr4/Cpp_data_reader/main.cpp
+++ antlr4/Cpp_data_reader/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "antlr4-runtime.h"
 #include "generated_cpp/CUSTOMParser.h"
 #include "generated_cpp/CUSTOMListener.h"
 #include "generated_cpp/CUSTOMLexer.h"
 #include "generated_cpp/CUSTOMBaseListener.h"
+
+class Loader : public CUSTOMBaseListener {
+public:
+  std::vector<std::string> rows;
+  virtual void exitField(CUSTOMParser::FieldContext *ctx) override { rows.push_back(ctx->TEXT()->getText());}
+
+};
+
 
 int main(int argc, const char* argv[]) {
   std::ifstream stream;
@@ -18,15 +28,16 @@ int main(int argc, const char* argv[]) {
     
   CUSTOMParser parser(&tokens);
   antlr4::tree::ParseTree *tree = parser.file();
-  std::cout << "Parser = " << tree->toStringTree(&parser) 
-            << std::endl;
-  CUSTOMBaseListener listener;
-  antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
 
-/*
-  for (auto row : listener.rows()) {
-    std::cout << row->toString() << std::endl;
+  std::cout << "Parser = " << tree->toStringTree(&parser) << std::endl;
+    
+  //CUSTOMBaseListener listener;
+  Loader loader;
+  //antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+  antlr4::tree::ParseTreeWalker::DEFAULT.walk(&loader, tree);
+
+   for (auto row : loader.rows) {
+       std::cout << row << std::endl;
   }
-*/
   return 0;
 }
